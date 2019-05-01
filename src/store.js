b319d3632e0e4edc8af2e6b49e9f09f6f6268d50
Mark Abeto <mac21macky@gmailcom>
2019-05-01 19:49:27 +0800

    Add 'state' properties and mutation logic 'ADD_OPERATION' and one action 'addNewOperation'

--- src/store.js
+++ src/store.js
@@ -1,16 +1,48 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { equalsLogic } from './helpers';
+
+
 
 Vue.use(Vuex)
 
+export const NUM_REGEX = /[0-9]/, SIGN_REGEX = /[×÷+-]/;
+
 export default new Vuex.Store({
   state: {
-
+    ops: [],
+    value: '0',
+    isMathSignClicked: false
   },
   mutations: {
+    ADD_OPERATION(state, payload) {
+      if (state.isMathSignClicked && NUM_REGEX.test(payload) && !SIGN_REGEX.test(payload)) {
+        state.value = payload;
+        state.isMathSignClicked = false;
+      }
+      else if (NUM_REGEX.test(payload) && state.value != '0') {
+        state.isMathSignClicked = false;
+        state.value += payload;
+      } else if (SIGN_REGEX.test(payload) && !state.isMathSignClicked) {
+        state.isMathSignClicked = true;
+        state.ops.push(state.value, payload);
+      }
+      else if (state.value == '0') {
+        state.value = payload;
+      } else if (payload == 'x²') {
 
+      }
+      else if (payload == '=') {
+        state.ops.push(state.value);
+        const val = equalsLogic(state.ops, state.value);
+        state.value = val;
+      }
+
+    }
   },
   actions: {
-
+    addNewOperation(ctx, symbol) {
+      ctx.commit('ADD_OPERATION', symbol);
+    }
   }
 })
